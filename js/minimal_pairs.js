f05e6eec70b8fccb66e404c18c823a88daa072c8
kuuuube <hexagonisalie@gmail.com>
2024-02-17 18:59:15 -0500

    Convert json to sane format

--- js/minimal_pairs.js
+++ js/minimal_pairs.js
@@ -49,13 +49,13 @@ function update_history(correct_answer, is_correct_answer) {
 function update_answer_buttons(json_data) {
     document.getElementById("answer-button-row").innerHTML = "";
     json_data["pairs"].forEach(function(currentValue, index, _) {
-        let raw_pronunciation = currentValue["entries"][0]["pronunciations"][0]["phrases"][0]["rawPronunciation"];
-        let accented_mora = currentValue["entries"][0]["pronunciations"][0]["phrases"][0]["accentedMora"];
+        let raw_pronunciation = currentValue["rawPronunciation"];
+        let accented_mora = currentValue["accentedMora"];
         let entry = output_accent_plain_text(raw_pronunciation, accented_mora);
         let new_answer_button = '<div class="col">\n<div class="d-grid"><button type="button" class="btn btn-primary" onclick="submit_answer(' + index + ')">' + entry + '</button></div>\n</div>';
         document.getElementById("answer-button-row").innerHTML += new_answer_button;
 
-        let sound_data = currentValue["entries"][0]["pronunciations"][0]["soundData"];
+        let sound_data = currentValue["soundData"];
         let button_sound_player = '<audio id="audio_index_' + index + '"src="data:audio/ogg;base64,' + sound_data + '" type="audio/mpeg"></audio>';
         if (index === current_correct_answer_button) {
             graded_answer_button_row += '<div class="col">\n<div class="d-grid">' + button_sound_player + '<button type="button" class="btn btn-success" onclick="document.getElementById(\'audio_index_' + index + '\').play()">' + entry + '</button></div>\n</div>';
@@ -68,15 +68,15 @@ function update_answer_buttons(json_data) {
 function update_audio(json_data) {
     let pairs_audio = {};
     json_data["pairs"].forEach(function(currentValue, index, _) {
-        pairs_audio[index] = currentValue["entries"][0]["pronunciations"][0]["soundData"];
+        pairs_audio[index] = currentValue["soundData"];
     });
-    let sound_data = json_data["pairs"][current_correct_answer_button]["entries"][0]["pronunciations"][0]["soundData"];
+    let sound_data = json_data["pairs"][current_correct_answer_button]["soundData"];
     document.getElementById("pair-sound-player").src = "data:audio/ogg;base64," + sound_data;
 }
 
 function set_pitch(json_data) {
     let pitch_accent = json_data["pairs"][current_correct_answer_button]["pitchAccent"];
-    let moracount = json_data["pairs"][current_correct_answer_button]["entries"][0]["moraCount"];
+    let moracount = json_data["pairs"][current_correct_answer_button]["moraCount"];
     if (pitch_accent == 0 || pitch_accent == moracount) {
         active_pitch_type = "heiban";
     } else if (pitch_accent == 1) {
@@ -122,8 +122,8 @@ async function fetch_random_pair() {
 
     current_correct_answer_button = Math.floor(Math.random() * (json_data["pairs"].length));
 
-    let raw_pronunciation = json_data["pairs"][current_correct_answer_button]["entries"][0]["pronunciations"][0]["phrases"][0]["rawPronunciation"];
-    let accented_mora = json_data["pairs"][current_correct_answer_button]["entries"][0]["pronunciations"][0]["phrases"][0]["accentedMora"];
+    let raw_pronunciation = json_data["pairs"][current_correct_answer_button]["rawPronunciation"];
+    let accented_mora = json_data["pairs"][current_correct_answer_button]["accentedMora"];
     current_correct_answer = output_accent_plain_text(raw_pronunciation, accented_mora);
 
     document.getElementById("kana-text").innerHTML = json_data["kana"];
