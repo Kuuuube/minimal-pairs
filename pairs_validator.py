1a425a0178af7e284dcfbbc0df4d8f00e3b01da7
kuuuube <hexagonisalie@gmail.com>
2024-12-22 15:23:26 -0500

    Deduplicate result writing

--- pairs_validator.py
+++ pairs_validator.py
@@ -39,38 +39,17 @@ for filename in os.listdir(dir_path):
     if len(set(map(str, sorted(silenced_moras)))) > 1:
         pair_silenced_moras_mismatches.append(filename + " [" + ", ".join(map(str, silenced_moras)) + "]")
 
-if len(pair_raw_pronunciation_mismatches) > 0:
-    sys.stdout.write("\x1b[1;31m") #bold red
-    print("Pair rawPronunciation mismatches:")
-    print(", ".join(pair_raw_pronunciation_mismatches))
-else:
-    sys.stdout.write("\x1b[1;32m") #bold green
-    print("Pair rawPronunciation test passed")
-sys.stdout.write("\x1b[1;0m") #reset
+def write_results(result, text):
+    if len(result) > 0:
+        sys.stdout.write("\x1b[1;31m") #bold red
+        print("Pair " + text + " mismatches:")
+        print(", ".join(result))
+    else:
+        sys.stdout.write("\x1b[1;32m") #bold green
+        print("Pair " + text + " test passed")
+    sys.stdout.write("\x1b[1;0m") #reset
 
-if len(pair_mora_count_mismatches) > 0:
-    sys.stdout.write("\x1b[1;31m") #bold red
-    print("Pair moraCount mismatches:")
-    print(", ".join(pair_mora_count_mismatches))
-else:
-    sys.stdout.write("\x1b[1;32m") #bold green
-    print("Pair moraCount test passed")
-sys.stdout.write("\x1b[1;0m") #reset
-
-if len(pair_silenced_moras_mismatches) > 0:
-    sys.stdout.write("\x1b[1;31m") #bold red
-    print("Pair silencedMoras mismatches:")
-    print(", ".join(pair_silenced_moras_mismatches))
-else:
-    sys.stdout.write("\x1b[1;32m") #bold green
-    print("Pair silencedMoras test passed")
-sys.stdout.write("\x1b[1;0m") #reset
-
-if len(pair_bad_sound_encoding) > 0:
-    sys.stdout.write("\x1b[1;31m") #bold red
-    print("Pair soundData bad base64 encoding:")
-    print(", ".join(pair_bad_sound_encoding))
-else:
-    sys.stdout.write("\x1b[1;32m") #bold green
-    print("Pair soundData encoding test passed")
-sys.stdout.write("\x1b[1;0m") #reset
+write_results(pair_raw_pronunciation_mismatches, "rawPronunciation")
+write_results(pair_mora_count_mismatches, "moraCount")
+write_results(pair_silenced_moras_mismatches, "silencedMoras")
+write_results(pair_bad_sound_encoding, "soundData base64 encoding")
